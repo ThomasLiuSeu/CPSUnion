1321d84021ff094e3b1e25c63a05c77cb880d1dd
wang qian <wq19901103wq@163.com>
2015-09-11 21:22:35 +0800

    order

--- api/python/cases/55haitao/fiftyfive_order.py
+++ api/python/cases/55haitao/fiftyfive_order.py
@@ -5,19 +5,25 @@ import sys;
 sys.path.append("/var/www/html/CPSUnion/api/python/common") 
 from httpclient import GetResponse
 from order import Order
+import time
 
-class FiftyFiveOrder(Order):
+class FiftyFiveOrder():
   def __init__(self):
-    params_list={'token':'ZhZIC5HluW3vVdlO','begin_date':'2014-01-01','end_date':'2015-08-01'}
+    params_list={'token':'ZhZIC5HluW3vVdlO','begin_date':'2014-01-01','end_date':'2015-10-01'}
     url="union.55haitao.com"
     suffix= "/union.php?mod=api_cashback"
     data = GetResponse(params_list,url, suffix)
-    decodejson = json.loads(data)
+    print  data;
+    decodejson  = json.loads(data)
     if decodejson["status"]["code"] == 0 :
-      print decodejson["data"]
+      orders_data = decodejson["data"]
+      for order_data in orders_data:
+        print order_data;
+        order = Order();
+        order.InsertDb(order_data["order_id"], order_data["tagcode"], 1, order_data["store_id"], order_data["store_name"], order_data["sale_amount"], order_data["cashback"], order_data["order_time"], order_data["report_time"], order_data["valid_time"], order_data["currency_type"],  order_data["status"]);
+        order.ShowDb();
     else :
       print decodejson["status"]
 
 if __name__ == '__main__':
-  order = FiftyFiveOrder();
-  order.ShowDb();
+  fiftyfiveorder = FiftyFiveOrder();
--- api/python/common/order.py
+++ api/python/common/order.py
@@ -10,11 +10,14 @@ class Order(object):
     data = cur.fetchall()
     for i in data:
       print i
-  def  InsertDb(self):
+
+  def  InsertDb(self, order_id, click_id, upper_union_id, store_id, store_name, trading_volume, cashback, order_time, report_time, valid_time, currency_type, status):
     conn = MySQLdb.connect('localhost','root','abcdefg2008','CPSUnion',charset='utf8')
     cur = conn.cursor()
-    sql_content = "select * from orders"
+    sql_content = "INSERT INTO `orders`  values(" + bytes(order_id) + "," + bytes(click_id) + "," + bytes(upper_union_id) + "," + bytes(store_id) + ",\"" + store_name + "\"," + bytes(trading_volume) + ","  + bytes(cashback) + "," + bytes(order_time) + "," + bytes(report_time)+ "," + bytes(valid_time) + ", \"" + currency_type  +"\"," + bytes(status )+ " );";
+    print sql_content;
     cur.execute(sql_content)
+    conn.commit()
     data = cur.fetchall()
     for i in data:
       print i
