8e91b920e64648af5f44af8559eefaf89f95da3d
wang qian <wq19901103wq@163.com>
2015-09-13 20:43:25 +0800

    api

--- api/python/common/order.py
+++ api/python/common/order.py
@@ -14,7 +14,7 @@ class Order(object):
   def  InsertDb(self, order_id, click_id, upper_union_id, store_id, store_name, trading_volume, cashback, order_time, report_time, valid_time, currency_type, status):
     conn = MySQLdb.connect('localhost','root','abcdefg2008','CPSUnion',charset='utf8')
     cur = conn.cursor()
-    sql_content = "INSERT INTO `orders`  values(" + bytes(order_id) + "," + bytes(click_id) + "," + bytes(upper_union_id) + "," + bytes(store_id) + ",\"" + store_name + "\"," + bytes(float(trading_volume) *100)+ ","  + bytes(float(cashback)*100*0.8) + "," + bytes(order_time) + "," + bytes(report_time)+ "," + bytes(valid_time) + ", \"" + currency_type  +"\"," + bytes(status )+ " );";
+    sql_content = "INSERT IGNORE INTO `orders`  values(" + bytes(order_id) + "," + bytes(click_id) + "," + bytes(upper_union_id) + "," + bytes(store_id) + ",\"" + store_name + "\"," + bytes(float(trading_volume) *100)+ ","  + bytes(float(cashback)*100*0.8) + "," + bytes(order_time) + "," + bytes(report_time)+ "," + bytes(valid_time) + ", \"" + currency_type  +"\"," + bytes(status )+ " );";
     print sql_content;
     cur.execute(sql_content)
     conn.commit()
