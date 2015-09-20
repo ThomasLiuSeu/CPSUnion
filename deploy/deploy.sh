f7f96ab4c4291ba1f7914c21228b9988f3fcdb96
Your Name <you@example.com>
2015-09-20 16:09:24 +0800

    online

---
+++ deploy/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+$projdir=/var/www/html
+cp ../*.html $projdir
+cp ../*.jsp $projdir
+cp -r ../webroot $projdir
+cp ../src/php/*.php $projdir
+cp -r ../document $projdir
+cd ..
+proj=`pwd`
+job="cd $proj/api/python/cases/55haitao && python fiftyfive_order.py"
+echo $job 
